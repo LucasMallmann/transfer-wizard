060cb61382de637dcd81bb8b2e7a5c56243d66c5
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-21 15:17:26 -0300

    feat: Added transaction migration and model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
+  @Column()
+  category: string;
 
+  // category_id: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
